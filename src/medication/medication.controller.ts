00dce66a75880d5a8c18deff95d403a23fd7978f
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-23 12:27:03 +0100

    cntroller to get medication detials

--- src/medication/medication.controller.ts
+++ src/medication/medication.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MedicationService } from './medication.service';
+// import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+// @UseGuards(JwtGuard)
+// @ApiSecurity('JWT-auth')
+@ApiTags('medication-endpoints')
 @Controller('medication')
-export class MedicationController {}
+export class MedicationController {
+  constructor(private medicationService: MedicationService) {}
+
+  @Get(':medicationId')
+  @ApiOperation({ summary: 'Get a patient by id' })
+  getPatientById(@Param('medicationId') medicationId: string) {
+    return this.medicationService.getMedicationDetailsById(medicationId);
+  }
+}
