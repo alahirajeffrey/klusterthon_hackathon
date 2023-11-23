5a24ec49eafa2138ffac05a390b6c4b048cf774b
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-23 11:19:49 +0100

    setup swagger and api

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // setup validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  // setup swagger
   const config = new DocumentBuilder()
     .setTitle('Klusterthon hackathon')
     .setVersion('1.0')
-    .addTag('Klusterthon hackathon')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/doc', app, document);
+  SwaggerModule.setup('api/v1/api-doc', app, document);
 
-  await app.listen(3001);
+  await app.listen(Number(process.env.NODE_PORT) || 3001);
 }
 bootstrap();
