28faaaf738f276328d4f3a8c15ecffb8bef0b2b1
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-23 11:22:16 +0100

    setup mongodb and winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,39 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from './common/config/config';
+import { MedicationModule } from './medication/medication.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(
+      // 'mongodb+srv://alahira:Xu4imxJxxdNh9gMQ@testcluster.vyyjz.mongodb.net/',
+      config.MONGO_URI,
+    ),
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      // options
+      transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({
+          dirname: 'logs',
+          filename: 'logs.log',
+        }),
+      ],
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp} ${level}: ${message}]`;
+        }),
+      ),
+    }),
+    MedicationModule,
+    PatientModule,
+  ],
 })
 export class AppModule {}
