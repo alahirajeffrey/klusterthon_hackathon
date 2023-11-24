7811af8d95962ea99f77f4254e3717eea1343a6b
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-24 13:03:58 +0100

    added otp schema and setup util & email service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,16 +5,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Patient, PatientShema } from 'src/schemas/patient.schema';
 import { Doctor, DoctorShema } from 'src/schemas/doctor.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { UtilService } from 'src/utils/util.service';
+import { EmailNotificationService } from 'src/email-notification/email-notification.service';
+import { Otp, OtpSchema } from 'src/schemas/otp.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Patient.name, schema: PatientShema },
       { name: Doctor.name, schema: DoctorShema },
+      { name: Otp.name, schema: OtpSchema },
     ]),
     JwtModule.register({}),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UtilService, EmailNotificationService],
 })
 export class AuthModule {}
