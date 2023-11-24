cfdcdd28a7ea460fc83d41a7335eeff06d9601e1
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-24 13:05:25 +0100

    added function to generate otp

--- src/utils/util.service.ts
+++ src/utils/util.service.ts
@@ -24,4 +24,12 @@ export class UtilService {
       HttpStatus.INTERNAL_SERVER_ERROR,
     );
   }
+
+  /**
+   * generates a one time password for user
+   * @returns : string containing one time password
+   */
+  generateOtp(): string {
+    return Math.floor(Math.random() * 899999 + 10000).toString();
+  }
 }
