579b99ed4da35b6238e5fe7b5218b04892747f56
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-24 13:04:57 +0100

    added otp schema

---
+++ src/schemas/otp.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type OtpDocument = HydratedDocument<Otp>;
+
+@Schema({ timestamps: true })
+export class Otp {
+  @Prop()
+  id: mongoose.Schema.Types.ObjectId;
+
+  @Prop({ type: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' } })
+  patientId: string;
+
+  @Prop({ type: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor' } })
+  doctorId: string;
+
+  @Prop()
+  otp: string;
+}
+
+export const OtpSchema = SchemaFactory.createForClass(Otp);
