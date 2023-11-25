567f1f9f0ffdd71b710e1a815decf6c1aeb426a7
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-25 09:28:24 +0100

    added field to patient schema

--- src/schemas/patient.schema.ts
+++ src/schemas/patient.schema.ts
@@ -14,6 +14,9 @@ export class Patient {
   @Prop({ default: false })
   isEmailVerified: boolean;
 
+  @Prop({ default: false })
+  isCurrentlyBeingTreated: boolean;
+
   @Prop()
   password: string;
 
